1ec2b40da29e06a8dd92f44fdc6007724a35ef43
nailuoGG <nailuogg@gmail.com>
2024-08-10 00:40:28 +0800

    auto add $PATH to ~/.recoll/recoll.conf

--- Casks/recoll.rb
+++ Casks/recoll.rb
@@ -1,8 +1,6 @@
 cask 'recoll' do
   version '1.38.1-20240516-375bcbc0'
   sha256 '5c15e9000666c7019d641e62e9a3f751c35f44cd0009da76cdf0e31eceb9353f'
-
-  url "https://www.recoll.org/downloads/macos/recoll-#{version}.dmg"
   name 'Recoll'
   desc 'Full-text search for your desktop.'
 
@@ -15,24 +13,24 @@ cask 'recoll' do
     regex(/href="recoll[._-]([\d.-]+[a-f0-9]+)\.dmg"/i)
   end
 
+  url "https://www.recoll.org/downloads/macos/recoll-#{version}.dmg"
+
   postflight do
     system_command 'xattr', args: ['-rd', 'com.apple.quarantine', "#{appdir}/Recoll.app"]
+
+    recollpath = ENV['PATH'].split(':').sort.join(':')
+    file_path = File.expand_path('~/.recoll/recoll.conf')
+    unless File.read(file_path).include?('recollhelperpath =')
+      File.open(file_path, 'a') { |f| f.puts "recollhelperpath = #{recollpath}" }
+    end
   end
 
-  caveats <<~EOS
-    For Recoll to find commands, set recollhelperpath in ~/.recoll/recoll.conf:
+  caveats do
+    path_environment_variable "#{appdir}/Recoll.app/Contents/MacOS"
+  end
 
-    Intel Mac:
-    echo "recollhelperpath = /usr/local/bin" >> ~/.recoll/recoll.conf
-
-    Apple Silicon Mac:
-    echo "recollhelperpath = /opt/homebrew/bin" >> ~/.recoll/recoll.conf
-
-    To use recoll and recollindex from the terminal, add Recoll.app to your PATH:
-    echo 'export PATH=/Applications/Recoll.app/Contents/MacOS:$PATH' >> ~/.zshrc
-
-    Adjust ~/.zshrc to your shell's config file if you use a different shell.
-  EOS
-
-  zap trash: []
+  zap trash: [
+    '~/.recoll',
+    '~/.config/Recoll.org'
+  ]
 end
