fade718507588aeb2350f3e1356b28245feee39d
nailuoGG <nailuogg@gmail.com>
2024-08-09 23:45:44 +0800

    add livecheck feature

--- Casks/recoll.rb
+++ Casks/recoll.rb
@@ -1,16 +1,22 @@
-cask "recoll" do
-  version "1.38.1-20240516-375bcbc0"
-  sha256 "5c15e9000666c7019d641e62e9a3f751c35f44cd0009da76cdf0e31eceb9353f"
+cask 'recoll' do
+  version '1.38.1-20240516-375bcbc0'
+  sha256 '5c15e9000666c7019d641e62e9a3f751c35f44cd0009da76cdf0e31eceb9353f'
 
-  url "https://www.lesbonscomptes.com/recoll/downloads/macos/recoll-#{version}.dmg"
-  name "Recoll"
-  desc "Full-text search tool"
-  homepage "https://www.lesbonscomptes.com/recoll/"
+  url "https://www.recoll.org/downloads/macos/recoll-#{version}.dmg"
+  name 'Recoll'
+  desc 'Recoll finds documents based on their contents as well as their file names.'
 
-  app "Recoll.app"
+  homepage 'https://www.recoll.org/'
+
+  app 'Recoll.app'
+
+  livecheck do
+    url 'https://www.recoll.org/downloads/macos/'
+    regex(/href="recoll[._-]([\d.-]+[a-f0-9]+)\.dmg"/i)
+  end
 
   postflight do
-    system_command "xattr", args: ["-rd", "com.apple.quarantine", "#{appdir}/Recoll.app"]
+    system_command 'xattr', args: ['-rd', 'com.apple.quarantine', "#{appdir}/Recoll.app"]
   end
 
   caveats <<~EOS
